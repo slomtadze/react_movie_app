0c8479de4427a764faf14dccf3af767e5d250120
Saba Lomtadze <slomtadze90@gmail.com>
2022-11-17 08:56:34 +0000

    Update 2 files

--- src/Pages/SignIn.jsx
+++ src/Pages/SignIn.jsx
@@ -1,12 +1,7 @@
-import bgImg from "../assets/backgroundIMG.jpg";
+import SignWrapper from "../Layout/SignIn/SignWrapper";
 
 const SignIn = () => {
-  return (
-    <div className="h-screen w-full relative">
-      <img src={bgImg} alt="movie img" className="h-full w-full cover center" />
-      <div className="h-full w-full bg-black/50 absolute opacity-0.5 top-0 left-0 z-5" />
-    </div>
-  );
+  return <SignWrapper />;
 };
 
 export default SignIn;
