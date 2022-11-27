aee8c009ddc2b87536c298bffcba874aed3914d9
Slomtadze <slomtadze90@gmail.com>
2022-11-27 19:16:55 +0400

    added "add an remove" functions for liked movies

--- src/components/MovieCart.js
+++ src/components/MovieCart.js
@@ -1,13 +1,38 @@
-import { Fragment, useState } from "react";
+import { Fragment, useContext, useState } from "react";
 import { imgBase } from "../Utils/RequestURL";
 import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
 import { useNavigate } from "react-router-dom";
+import AuthContext from "../Context/Auth-context";
+import { db } from "../firebase.config";
+import { doc, updateDoc, arrayUnion, arrayRemove } from "firebase/firestore";
 
 const MovieCart = ({ movie }) => {
   const [isLiked, setIsLiked] = useState(false);
+  const { user } = useContext(AuthContext);
   const navigate = useNavigate();
 
+  const addToFavorites = async (movieTitle) => {
+    const userRef = doc(db, "users", user.email);
+    await updateDoc(userRef, {
+      favorites: arrayUnion(movieTitle),
+    });
+  };
+
+  const removeFromFavorites = async (movieTitle) => {
+    const userRef = doc(db, "users", user.email);
+    await updateDoc(userRef, {
+      favorites: arrayRemove(movieTitle),
+    });
+  };
+
   const toggleLike = () => {
+    if (user) {
+      if (isLiked) {
+        removeFromFavorites(movie);
+      } else {
+        addToFavorites(movie);
+      }
+    }
     setIsLiked((isLiked) => !isLiked);
   };
 
